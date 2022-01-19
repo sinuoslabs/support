c7b77375e7b15da0fe8cf8ad8e9155005993e1c1
Houssene Dao <dao.houssene@gmail.com>
2022-01-19 10:52:26 +0000

    chore(int): cleanup content

--- lib/str/string.interface.ts
+++ lib/str/string.interface.ts
@@ -1,29 +1,3 @@
 export interface IString {
-  after(): this;
-  camel(value: string): this;
-  finish(): this;
-  is(): this;
-  slug(): this;
-  startWith(): this;
-  upper(value: string): this;
-  capitalize(value: string): this;
-  snake(): this;
-  studly(): this;
-  words(): this;
-  title(): this;
-  singular(): this;
-  plural(value: string, count: number): this;
-  start(): this;
-  random(length: number): this;
-  end(): this;
-  endWith(): this;
-  contains(): this;
-  before(): this;
-  lower(value: string): this;
-  l(value: string, encoding: string): this;
-  kebab(value: string): this;
-  limit(value: string, limit: number, end: string): this;
-  replaceFirst(): this;
-  replaceLast(): this;
-  ucFirst(value: string): this;
+  //
 }
