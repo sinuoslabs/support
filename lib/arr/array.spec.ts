f326848a6e4456e104ff895ed8c94f9afc0f0d15
Houssene Dao <dao.houssene@gmail.com>
2022-01-27 12:42:53 +0000

    feat(): add new method sort number

--- lib/arr/array.spec.ts
+++ lib/arr/array.spec.ts
@@ -42,4 +42,34 @@ describe('Arr', () => {
       expect(Arr.sort(string, (a, b) => b.localeCompare(a))).toEqual(['e', 'd', 'c', 'b', 'a']);
     });
   });
+
+  describe('sort number', () => {
+    it('should have sort method', () => {
+      expect(Arr).toHaveProperty('sort');
+    });
+
+    it('throw exception if parameter is empty', () => {
+      expect(() => {
+        Arr.sortNumber([]);
+      }).toThrow('You parameter can not be empty');
+      expect(() => {
+        Arr.sortNumber([]);
+      }).toThrowError(ArrException);
+    });
+
+    it('must use the sorting method to order in an asc', () => {
+      const array = [2, 1, 4, 10];
+
+      expect(Arr.sortNumber(array)).toEqual([1, 2, 4, 10]);
+      expect(Arr.sortNumber(array, 'asc')).toEqual([1, 2, 4, 10]);
+      expect(Arr.sortNumber(array, 'ASC')).toEqual([1, 2, 4, 10]);
+    });
+
+    it('must use the sorting method to order in an desc', () => {
+      const array = [2, 1, 4, 10];
+
+      expect(Arr.sortNumber(array, 'desc')).toEqual([10, 4, 2, 1]);
+      expect(Arr.sortNumber(array, 'DESC')).toEqual([10, 4, 2, 1]);
+    });
+  });
 });
--- lib/arr/array.ts
+++ lib/arr/array.ts
@@ -1,5 +1,7 @@
 import { ArrException } from '../exceptions';
 
+type order = 'asc' | 'ASC' | 'desc' | 'DESC';
+
 export const Arr = class {
   /**
    * Sort array.
@@ -12,5 +14,25 @@ export const Arr = class {
     if (array.length <= 0) throw new ArrException(`You parameter can not be empty`);
 
     return array.sort(fn);
+  }
+
+  /**
+   * Short array contain only number values.
+   *
+   * @param array
+   * @param order
+   */
+  static sortNumber(array: Array<number>, order: order = 'asc') {
+    if (!Array.isArray(array)) throw new ArrException(`The received parameter is not an array`);
+
+    if (array.length <= 0) throw new ArrException(`You parameter can not be empty`);
+
+    if (array.some(isNaN)) throw new ArrException(`Your array must contain only numeric values`);
+
+    if (order === 'asc' || order === 'ASC') {
+      return this.sort(array, (a, b) => a - b);
+    }
+
+    return this.sort(array, (a, b) => b - a);
   }
 };
